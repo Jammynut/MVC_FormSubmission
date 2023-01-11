cb8ea433e281ddafde7b676335f9b8d72ac2cf88
Nathan <nathandudley97@gmail.com>
2023-01-11 11:42:48 +0000

    Add project files.

---
+++ MVC_FormSubmission/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_FormSubmission.Models;
+using System;
+using System.Diagnostics;
+
+
+namespace MVC_FormSubmission.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+
+        [HttpPost]
+        public IActionResult Index(PersonModel person)
+        {
+            string firstName = person.FirstName;
+            string lastName = person.LastName;
+            string emailAddress = person.Email;
+            string phoneNumber = person.Phone;
+            string address = person.Address;
+
+            return View("Success");
+
+        }
+
+       public IActionResult Success()
+        {
+            return View();
+        }
+
+       
+
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+
+
+       
+    }
+}
---
+++ MVC_FormSubmission/Models/PersonModel.cs
@@ -0,0 +1,11 @@
+﻿namespace MVC_FormSubmission.Models
+{
+    public class PersonModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+    }
+}
